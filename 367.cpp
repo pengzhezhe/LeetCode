f44204b589096c7a895e7e5a03e9e8426e7b2c7b
pzz <448116718@qq.com>
2021-09-24 13:55:53 +0800

    solve 367. 有效的完全平方数

---
+++ 367.cpp
@@ -0,0 +1,43 @@
+//
+// Created by pzz on 2021/9/24.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left = 1, right = num / 2;
+
+        if (num == 1)
+            return true;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            int quotient = num / mid;
+            int remainder = num % mid;
+
+            if (quotient == mid) {
+                if (remainder == 0)
+                    return true;
+                left = mid + 1;
+            }
+            else if (quotient < mid)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return false;
+    }
+};
+
+int main() {
+    int num = 9;
+    Solution solution;
+    cout << solution.isPerfectSquare(num);
+
+}
