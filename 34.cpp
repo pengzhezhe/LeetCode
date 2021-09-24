42edbc7b53962faa5028d6dc27358c6f98c55166
pzz <448116718@qq.com>
2021-09-24 10:12:20 +0800

    solve 34.在排序数组中查找元素的第一个和最后一个位置

--- 34.cpp
+++ 34.cpp
@@ -8,56 +8,51 @@
 
 using namespace std;
 
-//class Solution {
-//public:
-//    vector<int> searchRange(vector<int> &nums, int target) {
-//        int left = 0, right = nums.size() - 1;
-//        int start = -1, end = -1;
-//
-//        while (left <= right) {
-//            int mid = (left + right) / 2;
-//            if (nums[mid] == target) {
-//                if (nums[mid - 1] == target && nums[mid + 1] == target)
-//                    right = mid + 1;
-//                if (nums[mid - 1] != target) {
-//                    start = mid;
-//                    left = mid + 1;
-//                }
-//                if (nums[mid + 1] != target) {
-//                    end = mid;
-//                    right = mid - 1;
-//                }
-//            } else if (nums[mid] < target)
-//                left = mid + 1;
-//            else
-//                right = mid - 1;
-//        }
-//        return vector<int>{start, end};
-//    }
-//};
-
 class Solution {
 public:
     vector<int> searchRange(vector<int> &nums, int target) {
         int left = 0, right = nums.size() - 1;
         int start = -1, end = -1;
 
+        if (nums.empty())
+            return vector<int>{-1, -1};
+
         while (left <= right) {
             int mid = (left + right) / 2;
 
-            if (nums[mid] > target)
+            if (nums[mid] >= target) {
                 right = mid - 1;
-            else
+                start = mid;
+            } else
                 left = mid + 1;
-
         }
+
+        if (start == -1)
+            return vector<int>{-1, -1};
+
+        if (nums[start] != target)
+            return vector<int>{-1, -1};
+
+        left = start;
+        right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] <= target) {
+                left = mid + 1;
+                end = mid;
+            } else
+                right = mid - 1;
+        }
+
         return vector<int>{start, end};
     }
 };
 
 int main() {
-    vector<int> nums{1};
-    int target = 1;
+    vector<int> nums{2, 2};
+    int target = 3;
     Solution s;
     vector<int> ans = s.searchRange(nums, target);
     for (int an: ans)
