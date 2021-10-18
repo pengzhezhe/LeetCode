efc5a23effc7b3db4ee34f3e8e36afcf29ad593f
pzz <448116718@qq.com>
2021-10-18 15:26:38 +0800

    solve: 142.环形链表II

---
+++ 142.cpp
@@ -0,0 +1,43 @@
+//
+// Created by pzz on 2021/10/18.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast) {
+                ListNode *index1 = fast;
+                ListNode *index2 = head;
+
+                while (index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index1;
+            }
+        }
+        return nullptr;
+    }
+};
+
+int main() {
+
+    return 0;
+}
