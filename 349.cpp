3e704492d244e4da3a41830060d82a86dbf0c7ba
pzz <448116718@qq.com>
2021-10-20 11:23:26 +0800

    solve: 349.两个数组的交集

---
+++ 349.cpp
@@ -0,0 +1,28 @@
+//
+// Created by pzz on 2021/10/20.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+        unordered_set<int> ans;
+        unordered_set<int> nums_set(nums1.begin(), nums1.end());
+        for (int num: nums2) {
+            if (nums_set.find(num) != nums_set.end())
+                ans.insert(num);
+        }
+
+        return vector<int>(ans.begin(),ans.end());
+    }
+};
+
+int main() {
+    return 0;
+}
