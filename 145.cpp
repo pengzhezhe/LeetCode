38c6f4fc369fe5ae2b618424d67851d1d352278c
pzz <448116718@qq.com>
2021-11-07 20:00:23 +0800

    add 145.二叉树的后序遍历(非递归)

--- 145.cpp
+++ 145.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -22,20 +23,51 @@ struct TreeNode {
 };
 
 
+//class Solution {
+//public:
+//    vector<int> postorderTraversal(TreeNode *root) {
+//        vector<int> result;
+//        postOrderTraversal(root, result);
+//        return result;
+//    }
+//
+//    void postOrderTraversal(TreeNode *root, vector<int> &result) {
+//        if (root == nullptr)
+//            return;
+//        postOrderTraversal(root->left, result);
+//        postOrderTraversal(root->right, result);
+//        result.push_back(root->val);
+//    }
+//};
+
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
+        stack<TreeNode *> stack;
         vector<int> result;
-        postOrderTraversal(root, result);
-        return result;
-    }
 
-    void postOrderTraversal(TreeNode *root, vector<int> &result) {
         if (root == nullptr)
-            return;
-        postOrderTraversal(root->left, result);
-        postOrderTraversal(root->right, result);
-        result.push_back(root->val);
+            return result;
+
+        stack.push(root);
+
+        while (!stack.empty()) {
+            TreeNode *node = stack.top();
+            result.push_back(node->val);
+            stack.pop();
+
+            if (node->left != nullptr) {
+                stack.push(node->left);
+            }
+
+            if (node->right != nullptr) {
+                stack.push(node->right);
+            }
+        }
+
+        reverse(result.begin(), result.end());
+        
+        return result;
     }
 };
 
