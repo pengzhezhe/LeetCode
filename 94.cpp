f5adce80d5a81171b9a1f7e77b40a78f0a49ee9d
pzz <448116718@qq.com>
2021-11-07 20:00:34 +0800

    add 94.二叉树的中序遍历(非递归)

--- 94.cpp
+++ 94.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -21,20 +22,42 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+//class Solution {
+//public:
+//    vector<int> inorderTraversal(TreeNode *root) {
+//        vector<int> result;
+//        inOrderTraversal(root, result);
+//        return result;
+//    }
+//
+//    void inOrderTraversal(TreeNode *root, vector<int> &result) {
+//        if (root == nullptr)
+//            return;
+//        inOrderTraversal(root->left, result);
+//        result.push_back(root->val);
+//        inOrderTraversal(root->right, result);
+//    }
+//};
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode *root) {
+        stack<TreeNode *> stack;
         vector<int> result;
-        inOrderTraversal(root, result);
-        return result;
-    }
+        TreeNode *cur = root;
 
-    void inOrderTraversal(TreeNode *root, vector<int> &result) {
-        if (root == nullptr)
-            return;
-        inOrderTraversal(root->left, result);
-        result.push_back(root->val);
-        inOrderTraversal(root->right, result);
+        while (cur != nullptr || !stack.empty()) {
+            if (cur != nullptr) {
+                stack.push(cur);
+                cur = cur->left;
+            } else {
+                cur = stack.top();
+                result.push_back(cur->val);
+                stack.pop();
+                cur = cur->right;
+            }
+        }
+        return result;
     }
 };
 
