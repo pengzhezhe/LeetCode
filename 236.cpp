a060d0925a679087a9b5ba176983a42bc2ecdb2c
pzz <448116718@qq.com>
2021-11-22 20:22:25 +0800

    solve 236.二叉树的最近公共祖先

---
+++ 236.cpp
@@ -0,0 +1,49 @@
+//
+// Created by pzz on 2021/11/22.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (root == p || root == q || root == nullptr)
+            return root;
+
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        if (left != nullptr && right != nullptr)
+            return root;
+
+        if (left != nullptr && right == nullptr)
+            return left;
+
+        if (left == nullptr && right != nullptr)
+            return right;
+
+        return nullptr;
+    }
+};
+
+int main() {
+    return 0;
+}
