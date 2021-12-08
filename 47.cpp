3215bbe857052bfdaf4400b0a76f0e0b8a941388
pzz <448116718@qq.com>
2021-12-08 22:09:41 +0800

    solve 47.全排列 II

--- 47.cpp
+++ 47.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -13,14 +14,17 @@ private:
     vector<vector<int>> result;
     vector<int> path;
     vector<bool> used;
-
     void backtracing(vector<int> &nums) {
         if (path.size() == nums.size()) {
             result.push_back(path);
             return;
         }
 
+        unordered_set<int> set;
         for (int i = 0; i < nums.size(); i++) {
+            if ((i > 0 && nums[i] == nums[i - 1]) && used[i - 1] == false) {
+                continue;
+            }
             if (used[i] == false) {
                 path.push_back(nums[i]);
                 used[i] = true;
@@ -32,7 +36,8 @@ private:
     }
 
 public:
-    vector<vector<int>> permute(vector<int> &nums) {
+    vector<vector<int>> permuteUnique(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
         used.insert(used.begin(), nums.size(), false);
         backtracing(nums);
         return result;
@@ -40,9 +45,9 @@ public:
 };
 
 int main() {
-    vector<int> nums = {1};
+    vector<int> nums = {1, 1, 2};
     Solution solution;
-    vector<vector<int>> result = solution.permute(nums);
+    vector<vector<int>> result = solution.permuteUnique(nums);
     for (auto v: result) {
         for (auto i: v) {
             cout << i;
