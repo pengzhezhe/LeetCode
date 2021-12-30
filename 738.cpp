5d46454d7835bbba7ff269e3995526d27d236a08
pzz <448116718@qq.com>
2021-12-30 12:11:38 +0800

    738. 单调递增的数字

---
+++ 738.cpp
@@ -0,0 +1,36 @@
+//
+// Created by pzz on 2021/12/30.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int monotoneIncreasingDigits(int n) {
+        string s = to_string(n);
+        int flag = s.size();
+        for (int i = s.size() - 1; i > 0; i--) {
+            if (s[i] < s[i - 1]) {
+                flag = i;
+                s[i - 1]--;
+            }
+        }
+
+        for (int i = flag; i < s.size(); i++) {
+            s[i] = '9';
+        }
+        
+        return stoi(s);
+    }
+};
+
+int main() {
+    Solution solution;
+    cout << solution.monotoneIncreasingDigits(658);
+    return 0;
+}
