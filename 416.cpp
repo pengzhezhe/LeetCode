18d12dd61240c82316554ee7bbc77acb9a13f030
pzz <448116718@qq.com>
2022-01-05 17:04:28 +0800

    416. 分割等和子集

--- 416.cpp
+++ 416.cpp
@@ -31,6 +31,37 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    bool canPartition(vector<int> &nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        vector<vector<int>> dp(nums.size(), vector<int>(sum / 2 + 1, 0));
+
+        for (int j = nums[0]; j <= sum / 2; j++) {
+            dp[0][j] = nums[0];
+        }
+
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j <= sum / 2; j++) {
+                if (j < nums[i])
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - nums[i]] + nums[i]);
+            }
+        }
+
+        return dp[nums.size() - 1][sum / 2] == sum / 2;
+    }
+};
+
 int main() {
     return 0;
 }
