5f382ca7f17ab36c9e7aa13ecd7350b07cf646f2
pzz <448116718@qq.com>
2022-03-08 16:42:32 +0800

    70. 爬楼梯 (安全背包解法)

--- 70.cpp
+++ 70.cpp
@@ -10,13 +10,30 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-        if ( n <= 1)
+        if (n <= 1)
             return n;
-        vector<int> dp(n+1);
+        vector<int> dp(n + 1);
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i <= n; i++)
             dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+};
+
+class Solution2 {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+        vector<int> cost{1, 2};
+        dp[0] = 1;
+
+        for (int j = 0; j <= n; j++) {
+            for (int i = 0; i < 2; i++) {
+                if (j >= cost[i])
+                    dp[j] += dp[j - cost[i]];
+            }
+        }
         return dp[n];
     }
 };
