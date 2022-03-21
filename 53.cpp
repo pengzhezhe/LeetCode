6999fc2cb0f1e03e33a28655447c95a0e70521e9
pzz <448116718@qq.com>
2022-03-21 16:26:02 +0800

    53. 最大子序和 (Dynamic planning)

--- 53.cpp
+++ 53.cpp
@@ -25,6 +25,20 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int maxSubArray(vector<int> &nums) {
+        vector<int> dp(nums.size() + 1, INT_MIN);
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            result = dp[i] > result ? dp[i] : result;
+        }
+        return result;
+    }
+};
+
 int main() {
     return 0;
 }
