9e9a1edae3e69978de4978485bd94eeffb6d3485
pzz <448116718@qq.com>
2022-06-08 10:51:02 +0800

    1037. 有效的回旋镖

---
+++ 1037.cpp
@@ -0,0 +1,22 @@
+//
+// Created by pzz on 2022/6/8.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>> &points) {
+        vector<int> v1 = {points[1][0] - points[0][0], points[1][1] - points[0][1]};
+        vector<int> v2 = {points[2][0] - points[0][0], points[2][1] - points[0][1]};
+        return v1[0] * v2[1] - v1[1] * v2[0] != 0;
+    }
+};
+
+int main() {
+    return 0;
+}
